9c2dc7847fc9c4f8b658185283db86a6e07713a5
kptnkrnch <jckptnkrnch@gmail.com>
2013-11-27 15:12:13 -0800

    Version-001

---
+++ packetize.h
@@ -0,0 +1,20 @@
+#ifndef PACKETIZE_H
+#define PACKETIZE_H
+
+#include <fstream>
+
+#define NUL  0x00
+#define SOT1 0x11
+#define SOT2 0x12
+#define SYN  0x16
+
+#define SEED 0xff
+
+#define PACKET_SIZE 1024
+#define DATA_SIZE PACKET_SIZE - 4
+
+bool recievePacket(unsigned char[]);
+void readFile(std::fstream&);
+void packetize(unsigned char[]);
+
+#endif
